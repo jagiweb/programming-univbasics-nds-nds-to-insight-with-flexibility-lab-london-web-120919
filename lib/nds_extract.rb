08567b87a41126a24bc992062106ce5392fec9ec
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-06 15:10:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -34,7 +34,7 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  puts movies_collection
+  p movies_collection
   # i = 0
   # while i < movies_collection.length do
   #   new_array.push(movies_collection[i])
